14cdfe3a2bb90518ec727946db1f0b6df27e9abe
Taras Chornyi <taras.chornyi.v@gmail.com>
2018-08-25 20:53:38 -0500

    Update isSmaller.js

--- FE_9_8_homework_functions/homework/src/isSmaller.js
+++ FE_9_8_homework_functions/homework/src/isSmaller.js
@@ -1,3 +1,14 @@
-function isSmiller(a, b) {
-    return !isBigger(a, b);
+// function isSmiller(a, b) {
+//     return !isBigger(a, b);
+// }
+
+function isSmaller(a, b) {
+    function isBigger(a, b) {
+        return a > b
+    }
+    if (a === b) {
+        return false
+    } else {
+        return !isBigger(a,b)
+    }
 }
