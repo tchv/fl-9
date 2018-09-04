79befd37597babd1495182bd1823c4391f98191d
Taras Chornyi <taras.chornyi.v@gmail.com>
2018-09-04 22:34:31 +0300

    root

---
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -0,0 +1,56 @@
+const one = 1;
+
+function get_todos() {
+    let todos = new Array();
+    let todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+ 
+function add() {
+    let task = document.getElementById('task').value;
+ 
+    let todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function remove() {
+    let id = this.getAttribute('id');
+    let todos = get_todos();
+    todos.splice(id, one);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function show() {
+    let todos = get_todos();
+    let check = '<i class="material-icons">check_box_outline_blank</i>';
+    let remove_start = '<i class="material-icons remove" id="';
+    let remove_end = '">delete</i>';
+ 
+    let html = '<ul>';
+    for(let i=0; i<todos.length; i++) {
+        html += '<li>' + check + '<span>' + todos[i] + '</span>' + remove_start + i + remove_end + '</li>';
+    }
+    html += '</ul>';
+ 
+    document.getElementById('todos').innerHTML = html;
+ 
+    let buttons = document.getElementsByClassName('remove');
+    for (let i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    }
+}
+ 
+document.getElementById('add').addEventListener('click', add);
+show();
